285e38f47c21087d044e45130606d818de3bc746
Saurabh Anand <saurabh.anand@3pillarglobal.com>
2020-10-30 20:03:50 +0530

    Seperaated detail component

--- src/App/Modules/Home/Home.js
+++ src/App/Modules/Home/Home.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
 //constant
 import axios from '../../../axios/index';
-import { CURRENCY, SELECT_UR_CURR_TXT, PERCENT_CHNAGE_TXT } from '../../constant';
+import { CURRENCY, SELECT_UR_CURR_TXT } from '../../constant';
 //material
-import { CssBaseline, Container, Grid, Typography, Box } from '@material-ui/core';
+import { CssBaseline, Container, Grid, Typography } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
 //styles
 import styles from './Home.module.css'
 import './Home.scss'
 // components
+import Detail from '../../Components/Detail'
 import CurrencyList from '../../Components/CurrencyList'
 
 function Home() {
@@ -75,58 +75,7 @@ function Home() {
                         data && data.coins && data.coins.length > 0
                             ? Object.values(data.coins).map((coin, index) => {
                                 return (
-                                    <div key={index} className="app_detail_card" style={{
-                                        borderTop: `24px solid ${coin.color ? coin.color : '#3f51b5'}`,
-                                        marginBottom: '48px'
-                                    }}>
-
-                                        <Grid className="app_detail_card_header" container direction="row" alignItems="flex-start" justify="space-between">
-                                            <Grid item>
-                                                <Box style={{  display: 'flex', alignItems: 'center'}}>
-                                                    <Box>
-                                                        <img src={coin.iconUrl} alt={`${coin.name}`} height="70px" />
-                                                    </Box>
-                                                    <Box>
-                                                        <Typography component="h2" variant="h2" className="coinName"><span>#{coin.rank}</span> {coin.name}</Typography>
-                                                    </Box>
-                                                </Box>
-                                            </Grid>
-                                            <Grid item>
-                                                <Box>
-                                                    <Box>
-                                                        <Typography component="p" variant="h2" color="secondary" className="app_detail_currentPrice" >
-                                                            {currencySign} {Math.round(coin.price).toLocaleString()}
-                                                        </Typography>
-                                                    </Box>
-                                                    <Box>
-                                                        <Typography component="p" align="center" variant="h5"  className="app_detail_change" >
-                                                            {PERCENT_CHNAGE_TXT} <span style={{
-                                                                color: coin.change && coin.change < 0 ? 'red' : 'green',
-                                                                fontWeight: 'bold'
-                                                            }}>{coin.change}%</span>
-                                                        </Typography>
-                                                    </Box>
-                                                </Box>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid className="app_detail_card_body">
-                                            <Grid item container direction="row" alignItems="flex-start" justify="space-between" xs="12" className="app_detail_card_body_boxes">
-                                                <Box >{coin.marketCap}<br /><span>Market Cap</span></Box>
-                                                <Box >{coin.marketCap}<br /><span>Market Cap</span></Box>
-                                                <Box >{coin.marketCap}<br /><span>Market Cap</span></Box>
-                                                <Box >{coin.marketCap}<br /><span>Market Cap</span></Box>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid className="app_detail_card_footer" container direction="row" alignItems="center" justify="center">
-                                            <Link to={{
-                                                pathname: `/details:id=${coin.id}`,
-                                                detailsProps: {
-                                                    id: coin.id,
-                                                    currencySign: currencySign
-                                                }
-                                            }} > For more details </Link>
-                                        </Grid>
-                                    </div>
+                                    <Detail key={index} id={coin.id} src={coin.iconUrl} name={coin.name} rank={coin.rank} sign={currencySign} price={coin.price} change={coin.change} color={coin.color} marketCap={coin.marketCap} />
                                 )
                             })
                             : null
